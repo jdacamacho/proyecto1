6b82b21361cc4a6802b69d136b3fdff82ae92a29
jdacamacho <jdacamacho@unicauca.edu.co>
2023-10-28 18:21:58 -0500

    CAMBIO

---
+++ src/main/java/com/unicauca/proyecto1/frameworks/repositorios/formatoPP_ARepositorio/FormatoPP_ARepositoryJPA.java
@@ -0,0 +1,11 @@
+package com.unicauca.proyecto1.frameworks.repositorios.formatoPP_ARepositorio;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.unicauca.proyecto1.frameworks.repositorios.entidades.PP_A.PropuestaTrabajoGradoPP_AEntity;
+
+@Repository
+public interface FormatoPP_ARepositoryJPA extends CrudRepository<PropuestaTrabajoGradoPP_AEntity,Integer>{
+    
+}
