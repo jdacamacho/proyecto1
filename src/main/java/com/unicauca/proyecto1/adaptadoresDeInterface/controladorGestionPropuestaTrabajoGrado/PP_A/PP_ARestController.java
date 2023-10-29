5feedec18b090490c38830e3cbad4c527fa85fec
jdacamacho <jdacamacho@unicauca.edu.co>
2023-10-28 21:58:01 -0500

    controladores PP_A terminados, a espera de pruebas

--- src/main/java/com/unicauca/proyecto1/adaptadoresDeInterface/controladorGestionPropuestaTrabajoGrado/PP_A/PP_ARestController.java
+++ src/main/java/com/unicauca/proyecto1/adaptadoresDeInterface/controladorGestionPropuestaTrabajoGrado/PP_A/PP_ARestController.java
@@ -1,4 +1,4 @@
-package com.unicauca.proyecto1.adaptadoresDeInterface.controladorGestionPropuestaTrabajoGrado.TI_A;
+package com.unicauca.proyecto1.adaptadoresDeInterface.controladorGestionPropuestaTrabajoGrado.PP_A;
 
 import java.io.IOException;
 import java.nio.file.Path;
@@ -20,64 +20,65 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.unicauca.proyecto1.adaptadoresDeInterface.controladorGestionPropuestaTrabajoGrado.DTOPeticion.PropuestaTrabajoGradoPP_ADTOPeticion;
 import com.unicauca.proyecto1.adaptadoresDeInterface.controladorGestionPropuestaTrabajoGrado.DTOPeticion.PropuestaTrabajoGradoTI_ADTOPeticion;
 import com.unicauca.proyecto1.adaptadoresDeInterface.controladorGestionPropuestaTrabajoGrado.DTOPeticion.RevisionComiteDTOPeticion;
 import com.unicauca.proyecto1.adaptadoresDeInterface.controladorGestionPropuestaTrabajoGrado.DTOPeticion.RutaAprobadaADTOPeticion;
+import com.unicauca.proyecto1.adaptadoresDeInterface.controladorGestionPropuestaTrabajoGrado.DTORespuesta.PropuestaTrabajoGradoPP_ADTORespuesta;
 import com.unicauca.proyecto1.adaptadoresDeInterface.controladorGestionPropuestaTrabajoGrado.DTORespuesta.PropuestaTrabajoGradoTI_ADTORespuesta;
 import com.unicauca.proyecto1.adaptadoresDeInterface.controladorGestionPropuestaTrabajoGrado.DTORespuesta.RevisionComiteDTORespuesta;
-import com.unicauca.proyecto1.reglasDeNegocioAplicacion.PropuestaTrabajoGrado.TI_A.GestionarTI_ACUInt;
+import com.unicauca.proyecto1.reglasDeNegocioAplicacion.PropuestaTrabajoGrado.PP_A.GestionarPP_ACUInt;
 
 @RestController
-@RequestMapping("/apiPropuestasTI_A")
-public class TI_ARestController {
-    
-    private final GestionarTI_ACUInt gestionarPropuestaTI_ACU;
+@RequestMapping("/apiPropuestasPP_A")
+public class PP_ARestController {
+    private final GestionarPP_ACUInt gestionarPropuestaPP_ACU;
 
-    public TI_ARestController(GestionarTI_ACUInt gestionarPropuestaTI_ACU){
-        this.gestionarPropuestaTI_ACU = gestionarPropuestaTI_ACU;
+    public PP_ARestController(GestionarPP_ACUInt gestionarPropuestaPP_ACU){
+        this.gestionarPropuestaPP_ACU = gestionarPropuestaPP_ACU;
     }
 
     @GetMapping("/propuestas")
-    public Iterable<PropuestaTrabajoGradoTI_ADTORespuesta> listar(){
-        return this.gestionarPropuestaTI_ACU.listarPropuestas();
+    public Iterable<PropuestaTrabajoGradoPP_ADTORespuesta> listar(){
+        return this.gestionarPropuestaPP_ACU.listarPropuestas();
     }
 
     @GetMapping("/propuestasDirector")
-    public Iterable<PropuestaTrabajoGradoTI_ADTORespuesta> listarDirector(@RequestParam(name = "idDirector",required = false) Integer idDirector){
-        return this.gestionarPropuestaTI_ACU.listarPropuestasDirector(idDirector);
+    public Iterable<PropuestaTrabajoGradoPP_ADTORespuesta> listarDirector(@RequestParam(name = "idDirector",required = false) Integer idDirector){
+        return this.gestionarPropuestaPP_ACU.listarPropuestasDirector(idDirector);
     }
 
     @GetMapping("/propuestas/{id}")
-    public PropuestaTrabajoGradoTI_ADTORespuesta consultarPropuesta(@PathVariable int id ){
-       return this.gestionarPropuestaTI_ACU.consultarPropuesta(id);
+    public PropuestaTrabajoGradoPP_ADTORespuesta consultarPropuesta(@PathVariable int id ){
+       return this.gestionarPropuestaPP_ACU.consultarPropuesta(id);
     }
 
     @PostMapping("/propuestas")
-    public PropuestaTrabajoGradoTI_ADTORespuesta crearPropuestas(@RequestBody PropuestaTrabajoGradoTI_ADTOPeticion objPeticion){
-        return this.gestionarPropuestaTI_ACU.crearPropuesta(objPeticion);
+    public PropuestaTrabajoGradoPP_ADTORespuesta crearPropuestas(@RequestBody PropuestaTrabajoGradoPP_ADTOPeticion objPeticion){
+        return this.gestionarPropuestaPP_ACU.crearPropuesta(objPeticion);
     }
-    
+
     @PostMapping("/propuestasRevisionComite")
     public RevisionComiteDTORespuesta realizarRevision(@RequestBody RevisionComiteDTOPeticion objPeticion){
-        return this.gestionarPropuestaTI_ACU.realizarRevision(objPeticion);
+        return this.gestionarPropuestaPP_ACU.realizarRevision(objPeticion);
     }
 
     @PutMapping("/propuestas")
-    public PropuestaTrabajoGradoTI_ADTORespuesta anexarDocumentoAprobado(@RequestBody RutaAprobadaADTOPeticion objPeticion){
-        return this.gestionarPropuestaTI_ACU.anexarPropuestaAprobado(objPeticion);
+    public PropuestaTrabajoGradoPP_ADTORespuesta anexarDocumentoAprobado(@RequestBody RutaAprobadaADTOPeticion objPeticion){
+        return this.gestionarPropuestaPP_ACU.anexarPropuestaAprobado(objPeticion);
     }
 
     @GetMapping("/propuestasSubidas/{idPropuesta}")
     public ResponseEntity<Resource> descargarPropuesta(@PathVariable int idPropuesta) throws IOException {       
-        if(this.gestionarPropuestaTI_ACU.existePropuesta(idPropuesta)){
-            PropuestaTrabajoGradoTI_ADTORespuesta propuesta =  this.gestionarPropuestaTI_ACU.consultarPropuesta(idPropuesta);
+        if(this.gestionarPropuestaPP_ACU.existePropuesta(idPropuesta)){
+            PropuestaTrabajoGradoPP_ADTORespuesta propuesta =  this.gestionarPropuestaPP_ACU.consultarPropuesta(idPropuesta);
             String rutaCompleta = propuesta.getRutaRespuestaPropuestaTrabajoGrado(); 
 
             Path path = Paths.get(rutaCompleta);
             Resource resource = new UrlResource(path.toUri());
 
             HttpHeaders headers = new HttpHeaders();
-            headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=formatoTI-A_Subido.docx"); 
+            headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=formatoPP-A_Subido.docx"); 
 
             MediaType mediaType = MediaType.parseMediaType("application/vnd.openxmlformats-officedocument.wordprocessingml.document");
 
@@ -102,15 +103,15 @@ public class TI_ARestController {
 
     @GetMapping("/propuestasAprobadas/{idPropuesta}")
     public ResponseEntity<Resource> descargarPropuestaAprobada(@PathVariable int idPropuesta) throws IOException {       
-        if(this.gestionarPropuestaTI_ACU.existePropuesta(idPropuesta)){
-            PropuestaTrabajoGradoTI_ADTORespuesta propuesta =  this.gestionarPropuestaTI_ACU.consultarPropuesta(idPropuesta);
+        if(this.gestionarPropuestaPP_ACU.existePropuesta(idPropuesta)){
+            PropuestaTrabajoGradoPP_ADTORespuesta propuesta =  this.gestionarPropuestaPP_ACU.consultarPropuesta(idPropuesta);
             String rutaCompleta = propuesta.getRutaRespuestaPropuestaTrabajoGrado(); 
 
             Path path = Paths.get(rutaCompleta);
             Resource resource = new UrlResource(path.toUri());
 
             HttpHeaders headers = new HttpHeaders();
-            headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=formatoTI-A_Aprobado.docx"); 
+            headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=formatoPP-A_Aprobado.docx"); 
 
             MediaType mediaType = MediaType.parseMediaType("application/vnd.openxmlformats-officedocument.wordprocessingml.document");
 
@@ -132,6 +133,4 @@ public class TI_ARestController {
                 .body(emptyResource);
         }
     }
-
-
-}   
+}
